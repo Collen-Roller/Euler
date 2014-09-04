3b063fd47551363fbb9195a0bb0f7156d0ad8437
Collen Roller <collen.roller@gmail.com>
2014-09-04 15:53:48 -0400

    euler 2 in c++

---
+++ Euler2/Test/Test.h
@@ -0,0 +1,18 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+
+class Test {
+    
+    public : 
+
+        Test();
+
+        ~Test();
+
+        void print_fib_numbers(int _max);
+
+};
+
+#endif
